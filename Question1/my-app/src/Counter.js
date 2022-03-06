492f697ba2ba50a514d269bbba5b16ed76f32d46
mahboob543 <mahboob6103@gmail.com>
2022-03-06 18:45:22 +0330

    send homework 14

--- Question1/my-app/src/Counter.js
+++ Question1/my-app/src/Counter.js
@@ -3,24 +3,27 @@ import Button from './Button';
 export default class Counter extends Component {
     constructor() {
         super();
-        this.state={
+        this.state = {
             count: 0
         }
-        
+
 
     }
-    increase=()=>{
-        this.setState({count:this.state.count+1})
+    increase = () => {
+        this.setState({ count: this.state.count + 1 })
     }
-    decrease=()=>{
-      this.setState({count:this.state.count-1})
+    decrease = () => {
+        if (this.state.count > 0) {
+            this.setState({ count: this.state.count - 1 })
+        }
+
     }
     render() {
         return (
             <div>
                 <h1>{this.state.count}</h1>
-                <Button title="+" handelClick={this.increase}/>
-                <Button title="-" handelClick={this.decrease}/>
+                <Button title="+" handelClick={this.increase} />
+                <Button title="-" handelClick={this.decrease} />
             </div>
         )
     }
